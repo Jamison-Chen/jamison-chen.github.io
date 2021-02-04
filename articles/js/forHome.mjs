fdc429b18688fee196fb4065b85171ed662026d7
Jamison Chen <jamisonforeverandever@gmail.com>
2021-02-04 21:40:00 +0800

    Add article home page.

---
+++ articles/js/forHome.mjs
@@ -0,0 +1,76 @@
+let windowWidth;
+let articlePreview = document.getElementsByClassName("preview");
+let classBar = document.getElementById("class-bar");
+let headerRightSide = document.getElementById("header-right-side");
+let searchBtn = document.getElementById("search-btn");
+let searchBar = document.getElementById("search-bar");
+let main = document.getElementById("main");
+// let lastScrollTop = 0;
+rwd();
+
+function rwd() {
+    windowWidth = window.innerWidth;
+    if (1024 <= windowWidth) {
+        main.classList.remove("col-10");
+        main.classList.remove("col-11");
+        main.classList.add("col-6");
+        classBar.classList.remove("col-10");
+        classBar.classList.remove("col-11");
+        classBar.classList.add("col-6");
+        classBar.classList.add("offset-3");
+        classBar.classList.add("d-flex");
+        headerRightSide.classList.remove("col-1");
+        headerRightSide.classList.add("col-3");
+    } else if (512 <= windowWidth && windowWidth < 1024) {
+        main.classList.remove("col-6");
+        main.classList.remove("col-11");
+        main.classList.add("col-10");
+        classBar.classList.remove("col-6");
+        classBar.classList.remove("col-11");
+        classBar.classList.remove("offset-3");
+        classBar.classList.add("col-10");
+        classBar.classList.add("offset-1");
+        classBar.classList.add("d-flex");
+        headerRightSide.classList.add("col-1");
+        headerRightSide.classList.remove("col-3");
+    } else if (windowWidth < 512) {
+        main.classList.remove("col-6");
+        main.classList.remove("col-10");
+        main.classList.add("col-11");
+        classBar.classList.remove("col-6");
+        classBar.classList.remove("d-flex");
+        headerRightSide.classList.remove("col-3");
+    }
+}
+
+window.addEventListener("resize", rwd);
+
+// window.addEventListener("scroll", function() {
+//     let st = window.pageYOffset || document.documentElement.scrollTop;
+//     if (st > lastScrollTop) {
+//         console.log("hi");
+//     } else {
+//         // upscroll code
+//     }
+//     lastScrollTop = st <= 0 ? 0 : st;
+// }, false);
+searchBtn.addEventListener("click", controlSearchBar);
+searchBar.addEventListener("keyup", search);
+
+function controlSearchBar() {
+    if (searchBar.style.width != 0 && searchBar.style.width != "0px") {
+        searchBar.style.width = "0px";
+        searchBar.style.border = "0px solid transparent";
+        searchBar.value = "";
+    } else {
+        searchBar.style.width = `${main.offsetWidth*0.8}px`;
+        searchBar.style.border = "2px solid #888";
+    }
+}
+
+function search(e) {
+    if (e.keyCode === 13) {
+        console.log(e.target.value);
+        return;
+    }
+}
