77dab45f98f90b695c357352b27123ed607e1606
Jamison Chen <jamisonforeverandever@gmail.com>
2021-02-07 03:11:40 +0800

    Update About Me page

--- js/forAbout.js
+++ js/forAbout.js
@@ -1,9 +1,11 @@
 let windowWidth;
 let cardGrid = document.getElementById("card-grid");
+let allMainCards = document.getElementsByClassName("main-card");
 let fundInfo = document.getElementById("fundamental-info");
 let specialization = document.getElementById("specialization");
 let workExp = document.getElementById("work-experience");
 let contact = document.getElementById("contact");
+let myImg = document.getElementById("my-img");
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.getElementById("back-bar").style.display = "none";
 } else {
@@ -14,31 +16,47 @@ applyRWD();
 
 function applyRWD() {
     windowWidth = window.innerWidth;
-    if (420 <= windowWidth) {
+    if (640 <= windowWidth) {
         cardGrid.style.width = "50%";
         cardGrid.classList.add("wide");
         cardGrid.classList.remove("narrow");
-        fundInfo.classList.add("wide");
-        fundInfo.classList.remove("narrow");
-        specialization.classList.add("wide");
-        specialization.classList.remove("narrow");
-        workExp.classList.add("wide");
-        workExp.classList.remove("narrow");
-        contact.classList.add("wide");
-        contact.classList.remove("narrow");
-    } else if (windowWidth < 420) {
-        cardGrid.style.width = "90%";
+        for (let i = 0; i < allMainCards.length; i++) {
+            allMainCards[i].classList.add("wide");
+            allMainCards[i].classList.remove("narrow");
+        }
+    } else if (windowWidth < 640) {
+        cardGrid.style.width = "60%";
         cardGrid.classList.add("narrow");
         cardGrid.classList.remove("wide");
-        fundInfo.classList.add("narrow");
-        fundInfo.classList.remove("wide");
-        specialization.classList.add("narrow");
-        specialization.classList.remove("wide");
-        workExp.classList.add("narrow");
-        workExp.classList.remove("wide");
-        contact.classList.add("narrow");
-        contact.classList.remove("wide");
+        for (let i = 0; i < allMainCards.length; i++) {
+            allMainCards[i].classList.add("narrow");
+            allMainCards[i].classList.remove("wide");
+        }
     }
 }
 
 window.addEventListener("resize", applyRWD);
+
+fundInfo.addEventListener("mouseenter", e => {
+    myImg.style.left = "-100%";
+    myImg.style.opacity = "0%";
+});
+fundInfo.addEventListener("mouseleave", e => {
+    myImg.style.left = "0";
+    myImg.style.opacity = "100%";
+});
+
+for (let i = 0; i < allMainCards.length; i++) {
+    allMainCards[i].addEventListener("mouseenter", e => {
+        try {
+            document.querySelector(`#${allMainCards[i].id}>.mask`).style.left = "100%";
+            document.querySelector(`#${allMainCards[i].id}>.mask`).style.opacity = "0%";
+        } catch (e) {}
+    });
+    allMainCards[i].addEventListener("mouseleave", e => {
+        try {
+            document.querySelector(`#${allMainCards[i].id}>.mask`).style.left = "0";
+            document.querySelector(`#${allMainCards[i].id}>.mask`).style.opacity = "80%";
+        } catch (e) {}
+    });
+}
