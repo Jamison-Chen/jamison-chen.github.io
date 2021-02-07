b5ca1fa7e9f3e03d99dee89efbdfa1e5b8386d0c
Jamison Chen <jamisonforeverandever@gmail.com>
2021-02-07 20:52:54 +0800

    Complete one article.

--- articles/articles/script.mjs
+++ articles/articles/script.mjs
@@ -2,6 +2,7 @@ let windowWidth;
 let articleTitle = document.getElementById("article-title");
 let articleInfo = document.getElementById("article-info");
 let articleContent = document.getElementById("article-content");
+let recommendedArticle = document.getElementById("recommended-article");
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.getElementById("back-bar").style.display = "none";
@@ -18,16 +19,19 @@ function applyRWD() {
         articleTitle.style.padding = "0 20vw"
         articleInfo.style.width = "60%";
         articleContent.style.width = "60%";
+        recommendedArticle.style.width = "60%";
     } else if (512 <= windowWidth && windowWidth < 1024) {
         articleTitle.style.fontSize = "2.4rem";
         articleTitle.style.padding = "0 10vw"
         articleInfo.style.width = "80%";
         articleContent.style.width = "80%";
+        recommendedArticle.style.width = "80%";
     } else if (windowWidth < 512) {
         articleTitle.style.fontSize = "1.8rem";
         articleTitle.style.padding = "0 5vw"
         articleInfo.style.width = "90%";
         articleContent.style.width = "80%";
+        recommendedArticle.style.width = "80%";
     }
 }
 
