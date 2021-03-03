a9bf95e7af78cf6e1918146e9e4175ec15b2561b
Jamison Chen <jamisonforeverandever@gmail.com>
2021-03-03 22:28:26 +0800

    Modification Modify the grid layout. And change the way of implementing rwd for pages using this grid layout.

--- js/forIndex.mjs
+++ js/forIndex.mjs
@@ -1,21 +1,18 @@
-let windowWidth;
-let btnGrid = document.getElementById("btn-grid");
+// let windowWidth;
+// let btnGrid = document.getElementById("btn-grid");
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.getElementById("profile-tab").style.right = "110%";
 }
 
-applyRWD();
+// applyRWD();
 
-function applyRWD() {
-    windowWidth = window.innerWidth;
-    if (1024 <= windowWidth) {
-        btnGrid.style.width = "35%";
-    } else if (512 <= windowWidth && windowWidth < 1024) {
-        btnGrid.style.width = "50%";
-    } else if (windowWidth < 512) {
-        btnGrid.style.width = "90%";
-    }
-}
+// function applyRWD() {
+//     windowWidth = window.innerWidth;
+//     if (1024 <= windowWidth) {
+//     } else if (512 <= windowWidth && windowWidth < 1024) {
+//     } else if (windowWidth < 512) {
+//     }
+// }
 
-window.addEventListener("resize", applyRWD);
+// window.addEventListener("resize", applyRWD);
--- side_project/games/script.js
+++ side_project/games/script.js
@@ -1,5 +1,5 @@
-let windowWidth;
-let btnGrid = document.getElementById("btn-grid");
+// let windowWidth;
+// let btnGrid = document.getElementById("btn-grid");
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.getElementById("home-footer").style.display = "none";
@@ -7,17 +7,14 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     document.getElementById("home-footer").style.display = "flex";
 }
 
-applyRWD();
+// applyRWD();
 
-function applyRWD() {
-    windowWidth = window.innerWidth;
-    if (1024 <= windowWidth) {
-        btnGrid.style.width = "35%";
-    } else if (512 <= windowWidth && windowWidth < 1024) {
-        btnGrid.style.width = "50%";
-    } else if (windowWidth < 512) {
-        btnGrid.style.width = "90%";
-    }
-}
+// function applyRWD() {
+//     windowWidth = window.innerWidth;
+//     if (1024 <= windowWidth) {
+//     } else if (512 <= windowWidth && windowWidth < 1024) {
+//     } else if (windowWidth < 512) {
+//     }
+// }
 
-window.addEventListener("resize", applyRWD);
+// window.addEventListener("resize", applyRWD);
